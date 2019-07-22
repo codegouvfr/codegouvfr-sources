da88d8be2656a98caaef7e1cf148fd95fbf26be2
Bastien <bastien.guerry@data.gouv.fr>
2019-07-22 22:41:36 +0200

    Implement sorting for the current set of columns
    Fix https://github.com/bzg/codegouvfr/issues/2

--- src/cljs/codegouvfr/core.cljs
+++ src/cljs/codegouvfr/core.cljs
@@ -14,36 +14,70 @@
 (re-frame/reg-event-db
  :initialize-db
  (fn [_ _]      
-   {:repos nil}))
+   {:repos        nil
+    :sort-by      :stars
+    :reverse-sort :true}))
 
 (re-frame/reg-event-db
  :update-repos
  (fn [db [_ repos]]
-   (assoc db :repos repos)))
+   (if repos (assoc db :repos repos))))
+
+(re-frame/reg-sub
+ :sort-by
+ (fn [db _] (:sort-by db)))
+
+(re-frame/reg-sub 
+ :reverse-sort
+ (fn [db _] (:reverse-sort db)))
+
+(re-frame/reg-event-db
+ :sort-by
+ (fn [db [_ k]]
+   (when (= k (:sort-by db))
+     (re-frame/dispatch [:reverse-sort]))
+   (assoc db :sort-by k)))
+
+(re-frame/reg-event-db
+ :reverse-sort
+ (fn [db _]
+   (assoc db :reverse-sort (not (:reverse-sort db)))))
 
 (re-frame/reg-sub
  :repos
  (fn [db _]
-   (:repos db)))
+   (let [repos (condp = @(re-frame/subscribe [:sort-by])
+                 :name   (sort-by :nom (:repos db))
+                 :forks  (sort-by :nombre_forks (:repos db))
+                 :stars  (sort-by :nombre_stars (:repos db))
+                 :issues (sort-by :nombre_issues_ouvertes (:repos db))
+                 ;; FIXME: intuitive enough to sort by length of desc?
+                 :desc   (sort #(compare (count (:description %1))
+                                         (count (:description %2)))
+                               (:repos db)) 
+                 (:repos db))]
+     (if @(re-frame/subscribe [:reverse-sort])
+       (reverse repos)
+       repos))))
 
 (defn current-page []
-  [:div {:class "container"}
-   [:table {:class "table"}
-    [:thead
-     [:tr
-      [:th "Nom"]
-      [:th "Description"]
-      [:th "Forks"]
-      [:th "Stars"]
-      [:th "Issues"]]]
-    [:tbody
-     (for [d @(re-frame/subscribe [:repos])]
-       [:tr
-        [:td [:a {:href (:repertoire_url d)} (:nom d)]]
-        [:td (:description d)]
-        [:td (:nombre_forks d)]
-        [:td (:nombre_stars d)]
-        [:td (:nombre_issues_ouvertes d)]])]]])
+  [:table {:class "table"}
+   [:thead
+    [:tr
+     [:th [:button {:on-click (fn [] (re-frame/dispatch [:sort-by :name]))} "Nom"]]
+     [:th [:button {:on-click (fn [] (re-frame/dispatch [:sort-by :desc]))} "Description"]]
+     [:th [:button {:on-click (fn [] (re-frame/dispatch [:sort-by :forks]))} "Forks"]]
+     [:th [:button {:on-click (fn [] (re-frame/dispatch [:sort-by :stars]))} "Stars"]]
+     [:th [:button {:on-click (fn [] (re-frame/dispatch [:sort-by :issues]))} "Issues"]]]]
+   [:tbody
+    (for [d @(re-frame/subscribe [:repos])]
+      ^{:key d}
+      [:tr
+       [:td [:a {:href (:repertoire_url d)} (:nom d)]]
+       [:td (:description d)]
+       [:td (:nombre_forks d)]
+       [:td (:nombre_stars d)]
+       [:td (:nombre_issues_ouvertes d)]])]])
 
 (defn main-class []
   (reagent/create-class
