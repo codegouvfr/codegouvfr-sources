27483289e573236916bb4643c11d24b39f03e3da
Bastien <bzg@gnu.org>
2019-10-01 12:06:51 +0200

    Bump version number

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 ;; SPDX-License-Identifier: EPL-2.0
 ;; License-Filename: LICENSES/EPL-2.0.txt
 
-(defproject codegouvfr "0.6.2"
+(defproject codegouvfr "0.6.4"
   :description "Frontend to display public sector source code repositories"
   :url "https://github.com/etalab/codegouvfr"
   :license {:name "Eclipse Public License - v 2.0"
