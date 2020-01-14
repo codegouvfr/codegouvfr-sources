fdc4e3c7927d0348f94582960997134e6f8cdc02
Bastien <bzg@gnu.org>
2020-01-14 20:07:54 +0100

    Add more tests

--- test/codegouvfr/test.clj
+++ test/codegouvfr/test.clj
@@ -4,14 +4,26 @@
 
 (ns codegouvfr.test
   (:require [clojure.test :refer :all]
-            [codegouvfr.config :as config]))
+            [codegouvfr.config :as config]
+            [codegouvfr.server :as server]
+            [clj-http.client :as http]))
 
-(deftest test-environment-variables
+(deftest environment-variables-exist
   (testing "Checking if all environment variables contain strings."
-    (is (and (string? (System/getenv "SMTP_HOST"))
-             (string? (System/getenv "SMTP_LOGIN"))
-             (string? (System/getenv "SMTP_PASSWORD"))
-             (string? (System/getenv "CODEGOUVFR_ADMIN_EMAIL"))
-             (string? (System/getenv "CODEGOUVFR_FROM"))
-             (string? (System/getenv "CODEGOUVFR_PORT"))))))
+    (is (string? (System/getenv "SMTP_HOST")))
+    (is (string? (System/getenv "SMTP_LOGIN")))
+    (is (string? (System/getenv "SMTP_PASSWORD")))
+    (is (string? (System/getenv "CODEGOUVFR_ADMIN_EMAIL")))
+    (is (string? (System/getenv "CODEGOUVFR_FROM")))
+    (is (string? (System/getenv "CODEGOUVFR_PORT")))))
 
+(defn test-url [url]
+  (is (= 200 (try (:status (http/get url))
+                  (catch Exception e nil)))))
+
+(deftest urls-are-reachable
+  (testing "Checking if URLs return the data we need."
+    (test-url server/repos-url)
+    (test-url server/orgas-url)
+    (test-url server/emoji-json-url)
+    (test-url server/annuaire-url)))
