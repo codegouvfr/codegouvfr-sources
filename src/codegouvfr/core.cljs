5d927bf134393bde9e7936331688a9b1cb65ba9d
Bastien Guerry <bastien.guerry@data.gouv.fr>
2022-08-02 18:16:24 +0200

    src/codegouvfr/core.cljs: Remove unused code

--- src/codegouvfr/core.cljs
+++ src/codegouvfr/core.cljs
@@ -118,16 +118,6 @@
   [key val]
   (.setItem (.-localStorage js/window) key (.stringify js/JSON (clj->js val))))
 
-(defn get-item
-  "Return the value of `key` from browser's localStorage."
-  [key]
-  (js->clj (.parse js/JSON (.getItem (.-localStorage js/window) key))))
-
-(defn remove-item!
-  "Remove the browser's localStorage value for the given `key`."
-  [key]
-  (.removeItem (.-localStorage js/window) key))
-
 (defn to-locale-date [^String s lang]
   (when (string? s)
     (.toLocaleDateString (js/Date. (.parse js/Date s)) lang)))
